0400e9dad54f7725c8c4e64265f652ac79760ba0
scott thomas <srwthomas1@gmail.com>
2022-02-18 10:42:57 +0100

    updated css

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,9 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+require 'json'
+require 'open-uri'
+
 page_number = 0
 
 loop do
@@ -13,16 +12,16 @@ loop do
   movies = open(url).read
   movie = JSON.parse(movies)
   movie_results = movie['results']
-  movie_poster = "https://image.tmdb.org/t/p/w500"
+  movie_poster = 'https://image.tmdb.org/t/p/w500'
 
   movie_results.each do |movie|
-  new_movie = Movie.create(title: movie['title'], overview: movie['overview'], poster_url: movie_poster + movie['poster_path'], rating: movie['vote_average'])
-  puts "#{new_movie.title} has been created!"
+    new_movie = Movie.create(title: movie['title'], overview: movie['overview'], poster_url: movie_poster + movie['poster_path'], rating: movie['vote_average'])
+    puts "#{new_movie.title} has been created!"
   end
 
-  if page_number == 4
+  if page_number == 5
     break
-    end
+  end
 end
 
 puts "#{Movie.count}"
