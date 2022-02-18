3241fc39cf058053782163458ff20589003fd15c
scott thomas <srwthomas1@gmail.com>
2022-02-18 17:33:00 +0100

    updated js plugin typed txt

--- app/javascript/controllers/typed_controller.js
+++ app/javascript/controllers/typed_controller.js
@@ -3,7 +3,7 @@ import Typed from 'typed.js';
 
 export default class extends Controller {
   connect() {
-    console.log("connnected")
+    // console.log("connnected")
     new Typed('#banner-typed-text', {
       strings: ["Add movies to your personal lists and rate them!"],
       typeSpeed: 100,
