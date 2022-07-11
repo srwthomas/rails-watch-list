d5bda7385d27c724cf0f2596351338fe8c6efe8b
scott thomas <srwthomas1@gmail.com>
2022-07-11 17:07:05 +0200

    changes pgdeploy

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,4 +2,5 @@ class Movie < ApplicationRecord
   has_many :bookmarks
   validates :title, uniqueness: true, presence: true
   validates :overview, presence: true
+  validates :rating, presence: true
 end
