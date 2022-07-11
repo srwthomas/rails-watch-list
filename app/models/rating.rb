b2cc086f0474fce79ee3385132c0f03f3732bcc6
scott thomas <srwthomas1@gmail.com>
2022-07-11 18:27:28 +0200

    extra comma + range in bracket

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -1,5 +1,4 @@
 class Rating < ApplicationRecord
-  belongs_to :movie
-
-  validates :rating, presence: true, range: 1,5
+  belongs_to :movie,
+  validates :rating, presence: true, range: {1,5}
 end
