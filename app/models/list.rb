8dd9860bdfb60b2fabdd17d35851406d31339ffc
scott thomas <srwthomas1@gmail.com>
2022-07-11 20:27:27 +0200

    new schema + migration

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,6 +1,7 @@
 class List < ApplicationRecord
   has_many :bookmarks, dependent: :destroy
   has_many :movies, through: :bookmarks
+  has_one :imageUrl
 
   validates :name, uniqueness: true, presence: true
   validates :imageUrl, presence: :true
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_18_143216) do
+ActiveRecord::Schema.define(version: 2022_07_11_182623) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,7 +21,6 @@ ActiveRecord::Schema.define(version: 2022_02_18_143216) do
     t.bigint "list_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "ratings"
     t.integer "rating"
     t.index ["list_id"], name: "index_bookmarks_on_list_id"
     t.index ["movie_id"], name: "index_bookmarks_on_movie_id"
