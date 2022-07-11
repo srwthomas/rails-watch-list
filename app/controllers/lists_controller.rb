efdede07d63fe772204ea68c95b911fc943cd647
scott thomas <srwthomas1@gmail.com>
2022-07-11 19:27:55 +0200

    remove before_action

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,5 +1,5 @@
 class ListsController < ApplicationController
-  before_action :create, only: [:index, :show, :new, :create]
+  # before_action :set_user, only: [:show]
 
   def index
     @lists = List.all
